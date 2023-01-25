23dcfae5a0b3498443847c480acb1eedf845f588
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-01-25 19:50:51 +0100

    js video function done

--- script.js
+++ script.js
@@ -1,65 +1,67 @@
-const video = document.getElementById('video');
-const play = document.getElementById('play');
-const stop = document.getElementById('stop');
-const progress = document.getElementById('progress');
-const timestamp = document.getElementById('timestamp');
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
 
 // Play & pause video
-function toggleVideoStatus() {
+const toggleVideoStatus = () => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
-}
+};
 
-// update play/pause icon
-function updatePlayIcon() {
+const updatePlayIcon = () => {
   if (video.paused) {
-    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    play.innerHTML = `<i class="fa fa-play fa-2x"></i>`;
   } else {
-    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`;
   }
-}
+};
 
-// Update progress & timestamp
-function updateProgress() {
+// Update progress & timeStamp
+const updateProgress = () => {
   progress.value = (video.currentTime / video.duration) * 100;
 
-  // Get minutes
+  // get minutes
+
   let mins = Math.floor(video.currentTime / 60);
   if (mins < 10) {
-    mins = '0' + String(mins);
+    mins = "0" + String(mins);
   }
 
-  // Get seconds
+  // get seconds
+
   let secs = Math.floor(video.currentTime % 60);
   if (secs < 10) {
-    secs = '0' + String(secs);
+    secs = "0" + String(secs);
   }
 
   timestamp.innerHTML = `${mins}:${secs}`;
-}
+};
 
 // Set video time to progress
-function setVideoProgress() {
+const setVideoProgress = () => {
   video.currentTime = (+progress.value * video.duration) / 100;
-}
+};
 
 // Stop video
-function stopVideo() {
+const stopVideo = () => {
   video.currentTime = 0;
   video.pause();
-}
+};
 
-// Event listeners
-video.addEventListener('click', toggleVideoStatus);
-video.addEventListener('pause', updatePlayIcon);
-video.addEventListener('play', updatePlayIcon);
-video.addEventListener('timeupdate', updateProgress);
+// Event listener
 
-play.addEventListener('click', toggleVideoStatus);
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
 
-stop.addEventListener('click', stopVideo);
+play.addEventListener("click", toggleVideoStatus);
 
-progress.addEventListener('change', setVideoProgress);
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
